89f74162a02f40af21ed2fa4abf8dec54b943ec1
王锦 <30552475+woaipuppyball@users.noreply.github.com>
2017-07-30 20:14:30 +0800

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,22 @@
+print"I will now count my chickens:"
+
+print"hens", 25 + 30/6
+print"roosters", 100 - 25*3%4
+
+print"Now I will count the egg:"
+
+print 3+2+1-5+4%2-1/4+6
+
+print "Is it true that 3+2 < 5-7?"
+
+print 3+2<5-7
+
+print"what is 3 + 2?",3+2
+print "What is 5-7?", 5-7
+
+print "that's why it is false."
+
+print"how about some more."
+print"Is it greater?",5>-2
+print"Is it greater or equal? ", 5 >= -2
+print "Is it less or equal?", 5 <= -2
